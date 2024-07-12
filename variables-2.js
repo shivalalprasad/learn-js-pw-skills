6804ddc7ffb9ad049c9d29cbb47ed2704ac9b7ad
Your Name <you@example.com>
2024-07-12 17:42:14 +0530

    added variables day 2

---
+++ variables-2.js
@@ -0,0 +1,20 @@
+// how to assin varibles and their values   
+
+// Let 
+
+let x = 111;
+
+console.log('this is the value assined to variable x =',x);
+
+// Var
+
+var y = 222;
+
+console.log('this is the value assined to variable y =',y);
+
+
+// Const
+
+const z = 333;
+
+console.log('this is the value assined to variable z =',z);

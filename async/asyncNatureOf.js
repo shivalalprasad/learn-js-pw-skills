59a5dd5c03284e0b33e3a3f942a2e0d29e7f0275
shivalalprasad04 <shivalalprasad04@gmail.com>
2024-08-27 17:23:07 +0000

    asyns

---
+++ async/asyncNatureOf.js
@@ -0,0 +1,17 @@
+console.log('set time out at 5000ms started');
+setTimeout(()=> {
+    console.log('set time out at 5000ms ended');
+}, 5000);
+
+
+//  here the both set timout are runs parellely with the help of event loop & runtime environment 
+
+// if we use any JavaScript native code that runs in call stack it blocks the event loop to execute the code that not natively present in JavaScript
+
+
+console.log('set time out at 2000ms started');
+setTimeout(()=>{
+    console.log('set time out at 2000ms ended');
+}, 2000);
+
+
---
+++ async/callbacks&hof.js
@@ -0,0 +1,13 @@
+const concepts =[`async`,`async/await`,`promise`,`fetch`,`then`,`try`,`catch`,`finally`]
+
+function myMap(arr,fn) {
+        let result = [];
+        for(let i = 0; i < arr.length; i++) {
+            result.push(fn(arr[i]));
+        }
+        return result;
+}
+
+console.log(myMap(concepts, function g(element) {
+    return element.toUpperCase();
+}))
